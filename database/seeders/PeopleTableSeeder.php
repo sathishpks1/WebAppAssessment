bffb936c1aad4624b7d7a3ba18c73e4ec3eb63b8
Sathish <sathishpks1@gmail.com>
2023-08-21 12:32:39 +0530

    Added migrations and seeders for tables

---
+++ database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
